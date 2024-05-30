cba97a9f347e5679d78f4060f3c275046a48d86a
Pavel Churkin <68502064+mrpavelius@users.noreply.github.com>
2024-05-30 22:11:17 +0300

    add: confirmation menu before exiting

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,22 +1,49 @@
 using IJunior.TypedScenes;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayButton()
+    [SerializeField]
+    private GameObject _confirmationWindow;
+
+    [SerializeField]
+    private Button _playButton;
+
+    [SerializeField]
+    private Button _viewRecordsButton;
+
+    [SerializeField]
+    private Button _exitButton;
+
+    public void StartGame()
     {
         Game.Load();
     }
 
-    public void ViewRecordsButton()
+    public void ViewRecords()
     {
         Records.Load();
     }
 
-    public void ExitButton()
+    public void QuitGame()
     {
-        Exit.Load();
+        _playButton.interactable = false;
+        _viewRecordsButton.interactable = false;
+        _exitButton.interactable = false;
+        _confirmationWindow.SetActive(true);
+    }
+
+    public void CloseApplication()
+    {
+        Application.Quit();
+    }
+
+    public void BackToGame()
+    {
+        _playButton.interactable = true;
+        _viewRecordsButton.interactable = true;
+        _exitButton.interactable = true;
+        _confirmationWindow.SetActive(false);
     }
 }
